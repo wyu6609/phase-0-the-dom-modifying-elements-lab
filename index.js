0e3ed44234e8c707466db20b5cd7978681cc27df
William <wy427@nyu.edu>
2021-12-24 22:07:58 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// 1 remove id = main
+const element = document.querySelector("body");
+const main = document.getElementById("main");
+element.removeChild(main);
+
+// create h1 with id = "victory"
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "William is the champion";
